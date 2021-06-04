978a1c0118b878b17da5296e6af1fbe60eb66b06
Hunter <hjkalbert@rocketmail.com>
2021-06-04 17:01:52 -0400

    Finishing touches

--- src/Components/SubComponents/Modal.js
+++ src/Components/SubComponents/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import "firebase/firestore";
 import useFirestore from "../../Hooks/useFirestore";
-import {makeStyles} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
@@ -17,7 +17,6 @@ const theme = createMuiTheme({
   },
 });
 
-
 const useStyles = makeStyles({
   customerDiv: {
     width: "50vw",
@@ -29,39 +28,48 @@ const useStyles = makeStyles({
     textAlign: "center",
     margin: "2%",
     padding: "2%",
-    borderRadius: '20px',
+    borderRadius: "20px",
     [theme.breakpoints.down("sm")]: {
       width: "85vw",
       marginLeft: "auto",
       marginRight: "auto",
-    }
+    },
   },
   dataDisplay: {
     marginTop: "2%",
     marginBottom: "2%",
-    width: "100%",
+    marginLeft: "2%",
+    marginRight: "2%",
+    width: "95%",
+    height: "95%",
+    paddingLeft: "2%",
+    textAlign: "left",
+    display: "flex",
+    flexDirection: "column",
     alignItems: "center",
-    justifyContent: 'center',
-    textAlign: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    borderRadius: '20px',
-    paddingBottom: '2%',
-    paddingTop: '2%',
-    paddingLeft: '2%',
-    paddingRight: '2%',
+    borderRadius: "20px",
+    backgroundColor: "snow",
   },
   dataDiv: {
     marginTop: "2%",
+    marginBottom: "2%",
+    textAlign: "center",
+    boxShadow: "0px 0px 2px",
+    width: "80%",
+    borderRadius: "10px",
+    padding: "1%",
   },
-})
+  HeroIcon: {
+    width: "2em"
+  },
+});
 
 const Modal = ({ selectedDiv, setSelectedDiv }) => {
   const handleClick = (e) => {
     if (e.target.classList.contains("backdrop")) setSelectedDiv(null);
   };
   const { docs } = useFirestore("Customer Address");
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className="backdrop" onClick={handleClick}>
@@ -74,27 +82,110 @@ const Modal = ({ selectedDiv, setSelectedDiv }) => {
         <div className={classes.customerDiv}>
           <div className={classes.dataDisplay}>
             <div className={classes.dataDiv}>
-              Appointment ID: {JSON.stringify(selectedDiv.id)}
+              <svg
+                className={classes.HeroIcon}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+                />
+              </svg>{" "}
+              <p>Appointment ID: {JSON.stringify(selectedDiv.id)}</p>
             </div>
             <div className={classes.dataDiv}>
+              <svg
+                className={classes.HeroIcon}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                ></path>
+              </svg>{" "}
+              <p>
               Contact Information:{" "}
-              {JSON.stringify(selectedDiv.payload.origin.fullName)}{" "}
-              {JSON.stringify(selectedDiv.payload.origin.Number)}
+                {JSON.stringify(selectedDiv.payload.origin.fullName)}{" "}
+                {JSON.stringify(selectedDiv.payload.origin.Number)}
+              </p>
             </div>
             <div className={classes.dataDiv}>
-              Origin Location:{" "}
-              {JSON.stringify(selectedDiv.payload.origin.Origin)}{" "}
-              {JSON.stringify(selectedDiv.payload.origin.OriginGeometry)}{" "}
+              <svg
+                className={classes.HeroIcon}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                ></path>
+              </svg>
+              <p>
+                Origin Location:{" "}
+                {JSON.stringify(selectedDiv.payload.origin.Origin)}{" "}
+                {JSON.stringify(selectedDiv.payload.origin.OriginGeometry)}{" "}
+              </p>
             </div>
             <div className={classes.dataDiv}>
-              Destination Location:{" "}
-              {JSON.stringify(selectedDiv.payload.destination.Destination)}{" "}
-              {JSON.stringify(
-                selectedDiv.payload.destination.DestinationGeometry
-              )}{" "}
+              <svg
+                className={classes.HeroIcon}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+              <p>
+                Destination Location:{" "}
+                {JSON.stringify(selectedDiv.payload.destination.Destination)}{" "}
+                {JSON.stringify(
+                  selectedDiv.payload.destination.DestinationGeometry
+                )}{" "}
+              </p>
             </div>
             <div className={classes.dataDiv}>
-              Items: {JSON.stringify(selectedDiv.payload.Furniture)}{" "}
+              <svg
+                className={classes.HeroIcon}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
+                ></path>
+              </svg>
+              <p>Items: {JSON.stringify(selectedDiv.payload.Furniture)} </p>
             </div>
           </div>
         </div>
